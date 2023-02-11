77647cd76998edd8f34459f1c645898cf2106b42
Simon Ingeson <44818+smonn@users.noreply.github.com>
2023-02-10 23:15:38 -0500

    chore: use pascal case for umd lib name

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,29 @@ import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 import packageJSON from "./package.json";
 
-function toCamelCase(str: string) {
-  return str.replace(/-([a-z])/g, (g) => g[1]?.toUpperCase() ?? "");
+const separators = new RegExp(/[-_/]+/, "g");
+const nonWord = new RegExp(/[^\w\s]/, "g");
+const firstLetterAfterSpace = new RegExp(/\s+(.)(\w*)/, "g");
+const word = new RegExp(/\w/);
+
+// From https://stackoverflow.com/questions/4068573/convert-string-to-pascal-case-aka-uppercamelcase-in-javascript
+function toPascalCase(text: string) {
+  return text
+    .toLowerCase()
+    .replace(separators, " ")
+    .replace(nonWord, "")
+    .replace(
+      firstLetterAfterSpace,
+      (_, $2: string, $3: string) => `${$2.toUpperCase() + $3}`
+    )
+    .replace(word, (s) => s.toUpperCase());
 }
 
 export default defineConfig({
   build: {
     lib: {
       entry: "src/index.ts",
-      name: toCamelCase(packageJSON.name),
+      name: toPascalCase(packageJSON.name),
       fileName: "index",
     },
   },
